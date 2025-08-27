f73ecd309a4b625c009c46c11d95e6886e50d2ad
Lineath <fran.montserrat.developer@gmail.com>
2025-08-27 18:55:46 +0200

    Generated initial project structure

---
+++ cached-inventory-service/src/test/java/com/fmontser/inventory/cached_inventory_service/CachedInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmontser.inventory.cached_inventory_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CachedInventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
